cf71515633e4a5370d8a55af2770b1d76c3437cb
Anna <afrodita-kts@mail.ru>
2022-08-09 21:56:34 +0300

    задача 42

---
+++ Z_42/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать 
+//десятичное число в двоичное.
+//45 -> 101101
+//3  -> 11
+//2  -> 10
+
+// 42 task
+int n = Convert.ToInt32(Console.ReadLine());
+string end = "";
+while (n > 0)
+{
+    int x = n % 2;
+    end = Convert.ToString(x) + end;
+    n /= 2;
+}
+Console.WriteLine(end);
+
+
